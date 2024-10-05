41a57f21d4d6091fa5aa579666b4f8757ec30ecc
Nikita Chalyi <nikita4556@gmail.com>
2024-10-05 21:50:29 +0300

    second commit

--- test/unit/sortByName.test.js
+++ test/unit/sortByName.test.js
@@ -1,6 +1,6 @@
 const sorting = require("../../app");
 
-describe("Books names test suit", () => {
+/*describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
     expect(
       sorting.sortByName([
@@ -13,5 +13,39 @@ describe("Books names test suit", () => {
       "Волшебник изумрудного города",
       "Гарри Поттер",
     ]);
+  });
+}),*/
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+    const output = sorting.sortByName(input);
+    expect(output).toEqual(expected);
+  });
+});
+
+describe("Without sorting", () => {
+  it("Books should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города"
+    ];
+    const expected = [
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города",
+      "Гарри Поттер"
+    ];
+    const output = sorting.sortByName(input);
+    expect(output).toEqual(expected);
   });
 });
